6823fb012f5df81cf3d93bf3e0bd39052059938d
nebulousnox38 <nebulousnox38@gmail.com>
2026-09-24 13:11:14 -0700

    Fixxxesssss

--- Content.Client/_Mono/FireControl/UI/FireControlWindow.xaml.cs
+++ Content.Client/_Mono/FireControl/UI/FireControlWindow.xaml.cs
@@ -43,7 +43,7 @@ public sealed partial class FireControlWindow : FancyWindow
         SelectImpulseButton.OnPressed += SelectImpulseWeapons;
         SelectGuidedButton.OnPressed += SelectGuidedWeapons;
         SelectGuidedLimitedButton.OnPressed += SelectGuidedLimitedWeapons;
-        // Triad - End
+        // End Triad
     }
 
     private void SelectAllWeapons(BaseButton.ButtonEventArgs args)
